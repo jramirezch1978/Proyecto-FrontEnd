8db01767ab13e32a6ae4bbd1a95b35ad5e588728
DESKTOP-HBV4KI1\jramirez <jarchiroque@indracompany.com>
2024-11-30 22:18:49 -0500

    Clases 2024-11-30

---
+++ HelloWorldApp/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Mundo programador';
+  saludo = 'Hola!!!!! PROGRAMADOR; tienes que ver lo que he construido';
+}
