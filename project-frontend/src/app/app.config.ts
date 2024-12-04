9eba030524a65551392bc7aa74c3e49d82969581
DESKTOP-HBV4KI1\jramirez <jarchiroque@indracompany.com>
2024-12-03 21:14:15 -0500

    Clases 2024-12-03

--- project-frontend/src/app/app.config.ts
+++ project-frontend/src/app/app.config.ts
@@ -4,11 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideHttpClient(),
-    provideClientHydration()]
+    provideAnimations(),
+    provideClientHydration()
+  ]
 };
